8b60a3be8f990e56df57c3d8ff8ddd24393bc55f
Chris Lundblad <lonewulf0328@gmail.com>
2020-02-22 23:47:10 -0500

    Add project files.

---
+++ Pokedex/Controllers/PokedexController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pokedex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokedexController : ControllerBase
+    {
+        private readonly ILogger<PokedexController> _logger;
+
+        public PokedexController(ILogger<PokedexController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
